0320597778fedf4d7c3cc509ec3a2be3225384a1
Ilya <zaytsevilia.a@gmail.com>
2022-03-23 22:19:54 +0300

    lesson2

---
+++ service/frontend/src/App.js
@@ -0,0 +1,64 @@
+import './App.css';
+import React from 'react';
+import axios from 'axios';
+import AuthorList from './components/Author.js'
+import Footer from './components/Footer.js'
+import Menu from "./components/Menu.js";
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            'authors': []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://127.0.0.1:8000/api/authors/')
+            .then(response => {
+                const authors = response.data
+                    this.setState(
+                    {
+                        'authors': authors
+                    }
+                )
+            }).catch(error => console.log(error))
+    }
+
+    render () {
+        return (
+            <div>
+                <Menu />
+                <AuthorList authors={this.state.authors} />
+                <Footer />
+            </div>
+        )
+    }
+}
+
+export default App;
+
+
+//function App() {
+//  return (
+//    <div className="App">
+//      <header className="App-header">
+//        <img src={logo} className="App-logo" alt="logo" />
+//        <p>
+//          Edit <code>src/App.js</code> and save to reload.
+//        </p>
+//        <a
+//          className="App-link"
+//          href="https://reactjs.org"
+//          target="_blank"
+//          rel="noopener noreferrer"
+//        >
+//          Learn React
+//        </a>
+//      </header>
+//    </div>
+//  );
+//}
+//
+//export default App;
---
+++ service/frontend/src/components/Menu.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Menu = () => {
+    return (
+        <table>
+            <th>
+                На главную |
+            </th>
+            <th>
+                Авторы |
+            </th>
+            <th>
+                Контакты |
+            </th>
+        </table>
+    )
+}
+
+export default Menu
